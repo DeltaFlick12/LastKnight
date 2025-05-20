02ccac5c5079d9d513a31b539db5898b64c600c0
DeltaFlick12 <pedrolucasgomesbr1212@gmail.com>
2025-05-19 23:03:08 -0300

    Sistema de PVE
    PVE funcionando no endless

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Menu from '@/components/Menu.vue'
 import ClassSelect from '@/components/ClassSelect.vue'
 import Options from '@/components/Options.vue'
+import Endless from '@/components/Endless.vue'
 import GameView from '@/components/GameView.vue'
 
 const routes = [
@@ -9,6 +10,7 @@ const routes = [
   { path: '/story', component: GameView },
   { path: '/class', component: ClassSelect },
   { path: '/options', component: Options },
+  { path: '/endless', component: Endless },
 ]
 
 const router = createRouter({
