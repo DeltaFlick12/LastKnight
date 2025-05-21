98ebfaf659f2b101c4cc50d9d049b8e2939de2ac
FilipeDSADS <filipeaugustodsds@gmail.com>
2025-05-21 20:30:46 -0300

    att temp
    att terporario

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Menu from '@/components/Menu.vue'
 import ClassSelect from '@/components/ClassSelect.vue'
+import Endless from '@/components/Endless.vue'
 import Options from '@/components/Options.vue'
 import Endless from '@/components/Endless.vue'
 import GameView from '@/components/GameView.vue'
@@ -8,6 +9,7 @@ import GameView from '@/components/GameView.vue'
 const routes = [
   { path: '/', component: Menu },
   { path: '/story', component: GameView },
+  { path: '/endless', component: Endless },
   { path: '/class', component: ClassSelect },
   { path: '/options', component: Options },
   { path: '/endless', component: Endless },
